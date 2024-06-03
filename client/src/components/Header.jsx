6826a20cdcd78ccc737fa9bc4f58916cdb596ba3
unknown <adryx3326@gmail.com>
2024-06-03 23:49:05 +0200

    hi

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 export function Headers() {
     return (
     <header>
-        <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} ><NavLink to='/'> Home </NavLink></motion.button>
+        <motion.button whileHover={{ scale: 1.1, width: 100, textAlign: 'left' }} whileTap={{ scale: 0.9 }} ><NavLink to='/'> Home </NavLink></motion.button>
         <motion.button><NavLink to='/competence'> Competence </NavLink></motion.button>
         <motion.button><NavLink to='/contact'> Contact </NavLink></motion.button>
     </header>
